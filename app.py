7d86c5b0c97472e416f900cc4423ea232e88765a
o9zs <arsenyojike@gmail.com>
2024-12-22 18:38:22 +0300

    Added bot deletion option

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ console = Console(highlight=False)
 options = {
 	1: "leave groups",	
 	2: "leave channels",
-	3: "delete conversations",
-	4: "clear favorites"
+	3: "delete bots",
+	4: "delete conversations",
+	5: "clear favorites"
 }
 
 console.log("\n".join([f"[bold]{number}[/bold] - {text}" for number, text in options.items()]))
@@ -33,6 +34,8 @@ async def main():
 
 				await asyncio.sleep(config.interval)
 
+		console.log("\n")
+
 	if "2" in selected_options:
 		console.log("\n[italic]Leaving channel...[/italic]")
 
@@ -44,26 +47,45 @@ async def main():
 
 				await asyncio.sleep(config.interval)
 
+		console.log("\n")
+
 	if "3" in selected_options:
+		console.log("\n[italic]Deleting bots...[/italic]")
+
+		async for dialog in client.iter_dialogs():
+			if dialog.is_user and dialog.entity.bot:
+				await client.delete_dialog(dialog)
+
+				console.log(f"Deleted bot [bold]{dialog.name}[/bold]")
+
+				await asyncio.sleep(config.interval)
+
+		console.log("\n")
+
+	if "4" in selected_options:
 		console.log("\n[italic]Deleting conversations...[/italic]")
 
 		async for dialog in client.iter_dialogs():
-			if dialog.is_private:
+			if dialog.is_user and not dialog.entity.bot:
 				await client.delete_dialog(dialog)
 
 				console.log(f"Deleted conversation with [bold]{dialog.name}[/bold]")
 
 				await asyncio.sleep(config.interval)
 
-	if "4" in selected_options:
+		console.log("\n")
+
+	if "5" in selected_options:
 		console.log("\n[italic]Clearing favorites...[/italic]")
 
-		async for message in client.iter_messages("@me"):
+		async for message in client.iter_messages("me"):
 			message.delete()
 
 			console.log(f"Delete message with ID [bold]{message.id}[/bold]")
 
 			await asyncio.sleep(config.interval)
 
+		console.log("\n")
+
 with client:
-    client.loop.run_until_complete(main())
+	client.loop.run_until_complete(main())
